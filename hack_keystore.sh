901115336dc2b7fda4114e3c7fe5138caace260a
Tomáš Hejátko <tomas.hejatko@gmail.com>
2019-04-24 11:27:12 +0200

    change hack to keep keystore on persistent store and copy it on every startup - no symlinks

--- hack_keystore.sh
+++ hack_keystore.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [[ ! -L /usr/share/elasticsearch/config/elasticsearch.keystore ]];then
+if [[ ! -f /usr/share/elasticsearch/config/elasticsearch.keystore ]];then
 	if [[ ! -f /usr/share/elasticsearch/data/elasticsearch.keystore ]];then
 		echo "Hacking keystore..."
 		/usr/share/elasticsearch/bin/elasticsearch-keystore create
-		mv /usr/share/elasticsearch/config/elasticsearch.keystore /usr/share/elasticsearch/data
+		chown elasticsearch:elasticsearch /usr/share/elasticsearch/config/elasticsearch.keystore
+		cp /usr/share/elasticsearch/config/elasticsearch.keystore /usr/share/elasticsearch/data
 	fi
-	echo "Create keystore symlink"
-	ln -s /usr/share/elasticsearch/data/elasticsearch.keystore /usr/share/elasticsearch/config/
+	echo "Copy keystore from persitent storage"
+	cp -a /usr/share/elasticsearch/data/elasticsearch.keystore /usr/share/elasticsearch/config/
 fi
